e476b79c9cd81f6169f78a6ca549f49a646cece3
coder-abdo <a.h2020.2007@gmail.com>
2018-07-05 17:16:31 +0200

    finishing all steps for that app

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input type="text" @input="onInput">
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput: function(event){
+            this.$emit('termChange', event.target.value);
+        }
+    }
+}
+</script>
+<style scoped>
+    input {
+        width: 75%;
+        border-radius: 3px;
+        border: 1px solid #e9ecef;
+        text-indent: 5px;
+    }
+    div {
+        text-align: center;
+        margin: 20px;
+    }
+</style>
+
